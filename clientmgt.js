7f6e9457a584614df75740924c412068e9d4dcaf
Kari Lentz <kari_lentz@amemusic.com>
2023-11-30 10:17:38 -0500

    addresses in addition to location screen

--- clientmgt.js
+++ clientmgt.js
@@ -1,6 +1,75 @@
 var express = require('express');
 var app = express();
 
+app.get('/addresses/:client_id', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'topdog',
+        server: 'database.amemusic.com', 
+        database: 'AmeMaster',
+        trustServerCertificate: true
+    };
+
+    try{
+
+        if(!req.params){
+            throw new Error("no parameters, please provide client-id at least");
+        }
+
+        if(req.params.client_id == ""){
+            throw new Error("no client-id paramter provided");
+        }
+
+        
+        var client_id = parseInt(req.params.client_id);
+        if(isNaN(client_id) || isNaN(parseFloat(client_id))){
+            throw new Error("client id not a number");
+        }
+    }
+    catch(e)
+    {
+        res.status(400).json({ message: e.message})
+        return;
+    }
+
+    try {
+
+        // connect to your database
+        sql.connect(config, function (err) {
+        
+                if (err) console.log(err);
+
+                // create Request object
+                var request = new sql.Request();
+                
+                // query to the database and get the records
+                request.query(`select address_id, company_id, company_type, company_name, branch_type, Branch,
+                Member, Attention, address_1, address_2, address_3, 
+                City, State, Country, Zip, cast(case when notes is not null then notes else '' end as varchar(MAX)) as Notes, 
+                case when ol.seq_num is not null then ol.seq_num else 4 end as sort_key  
+                from rpm_client_address ca 
+                left outer join rpm_option_list ol on ol.application = 'AMECLIENTMGT' and ol.option_type = 'BRANCH_TYPE' and ol.option_value = ca.branch_type 
+                WHERE company_type = 'C' AND company_id = ${client_id} 
+                order by sort_key, address_1`, 
+                    function (err, recordset) {
+                    
+                        if (err) console.log(err)
+
+                        // send records as a response
+                        res.send(recordset);
+                });
+    });
+    }
+    catch(e){
+        res.status(500).json({ message: e.message})
+        return;
+    }
+});
+
 app.get('/locations/:client_id', function (req, res) {
    
     var sql = require("mssql");
