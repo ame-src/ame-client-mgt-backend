2c424dff258f1108aaa0cf1901a2b09d1959d71c
Kari Lentz <kari_lentz@amemusic.com>
2023-12-04 11:06:29 -0500

    addresses/locations urls get summary address/location urls get details

--- clientmgt.js
+++ clientmgt.js
@@ -23,13 +23,13 @@ function register_route(app, url, params, sql_builder){
                 if(Object.keys(params).length > 0){
 
                     if(!req.params){
-                        throw new Error("no parameters, please provide client-id at least");
+                        throw new Error(`no parameters, please provide ${req.params[0]} at least`);
                     }
 
                     for(var param in params){
 
                         if(req.params[param] == ""){
-                            throw new Error("no client-id paramter provided");
+                            throw new Error(`no ${param} parameter provided`);
                         }
 
                         if(params[param] == "Int"){
@@ -81,7 +81,7 @@ register_route(app,
                 params => 
                     `select address_id, company_id, company_type, company_name, branch_type, Branch,
                         Member, Attention, address_1, address_2, address_3, 
-                        City, State, Country, Zip, cast(case when notes is not null then notes else '' end as varchar(MAX)) as Notes, 
+                        City, State, Country, Zip,  
                         case when ol.seq_num is not null then ol.seq_num else 4 end as sort_key  
                         from rpm_client_address ca 
                         left outer join rpm_option_list ol on ol.application = 'AMECLIENTMGT' and ol.option_type = 'BRANCH_TYPE' and ol.option_value = ca.branch_type 
@@ -89,19 +89,39 @@ register_route(app,
                         order by sort_key, address_1`);
 
 register_route(app,
+"address",
+{"address_id":"Int"},
+params => 
+    `select address_id, company_id, company_type, company_name, branch_type, Branch,
+        Member, Attention, address_1, address_2, address_3, 
+        City, State, Country, Zip, cast(case when notes is not null then notes else '' end as varchar(MAX)) as Notes, 
+        case when ol.seq_num is not null then ol.seq_num else 4 end as sort_key  
+        from rpm_client_address ca 
+        left outer join rpm_option_list ol on ol.application = 'AMECLIENTMGT' and ol.option_type = 'BRANCH_TYPE' and ol.option_value = ca.branch_type 
+        WHERE company_type = 'C' AND address_id = ${params.address_id} 
+        order by sort_key, address_1`);
+
+register_route(app,
                 "locations",
                 {"client_id":"Int"},
                 params => 
-                    `select l.location_id, l.address_id, l.bill_to_address_id, l.ship_to_address_id, l.bill_to_policy, 
-                    l.ship_to_policy, l.monthly_service_fee, l.months_per_bill_period, l.bill_ahead_days,
-                    l.ascap_rate_plan, l.service_start_date, l.qc_call_date, l.season_start, l.season_end,
-                    l.zone_id, l.system_grace_period, l.prev_bill_through_date, 
-                    cast(case when l.Notes is not null then l.Notes else '' end as varchar(MAX)) as Notes, 
-                    l.update_method, l.disc_threshold, l.update_method_reason, l.location_type, 
-                    l.location_status, b.billed_from_date, b.billed_through_date, b.paid_through_date,
-                    l.charge_template_id 
-                    FROM rpm_client_location l 
-                    INNER JOIN qry_location_billing_dates b ON l.location_id = b.location_id WHERE l.client_id = ${params.client_id}`);
+                    `select l.location_id, l.address_id
+                    FROM rpm_client_location l WHERE l.client_id = ${params.client_id}`);
+
+register_route(app,
+    "location",
+    {"location_id":"Int"},
+    params => 
+        `select l.location_id, l.address_id, l.bill_to_address_id, l.ship_to_address_id, l.bill_to_policy, 
+        l.ship_to_policy, l.monthly_service_fee, l.months_per_bill_period, l.bill_ahead_days,
+        l.ascap_rate_plan, l.service_start_date, l.qc_call_date, l.season_start, l.season_end,
+        l.zone_id, l.system_grace_period, l.prev_bill_through_date, 
+        cast(case when l.Notes is not null then l.Notes else '' end as varchar(MAX)) as Notes, 
+        l.update_method, l.disc_threshold, l.update_method_reason, l.location_type, 
+        l.location_status, b.billed_from_date, b.billed_through_date, b.paid_through_date,
+        l.charge_template_id 
+        FROM rpm_client_location l 
+        INNER JOIN qry_location_billing_dates b ON l.location_id = b.location_id WHERE l.location_id = ${params.location_id}`);
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
