f8cb034012d1b926c88fc9da896537839bfabe4c
Kari Lentz <kari_lentz@amemusic.com>
2023-12-04 08:27:05 -0500

    replace register_client_route with register_route

--- clientmgt.js
+++ clientmgt.js
@@ -10,28 +10,35 @@ var config = {
     trustServerCertificate: true
 };
 
-function register_client_route(app, url, sql_builder){
+function register_route(app, url, params, sql_builder){
 
     app.get(
-        url,
+        `/${url}/:${Object.keys(params).join(",")}`,
         function(req, res) {
 
             var sql = require("mssql");
 
             try{
 
-                if(!req.params){
-                    throw new Error("no parameters, please provide client-id at least");
-                }
+                if(Object.keys(params).length > 0){
 
-                if(req.params.client_id == ""){
-                    throw new Error("no client-id paramter provided");
-                }
+                    if(!req.params){
+                        throw new Error("no parameters, please provide client-id at least");
+                    }
 
-                
-                var client_id = parseInt(req.params.client_id);
-                if(isNaN(client_id) || isNaN(parseFloat(client_id))){
-                    throw new Error("client id not a number");
+                    for(var param in params){
+
+                        if(req.params[param] == ""){
+                            throw new Error("no client-id paramter provided");
+                        }
+
+                        if(params[param] == "Int"){
+                            var temp = parseInt(req.params[param]);
+                            if(isNaN(temp) || isNaN(parseFloat(temp))){
+                                throw new Error(`${param} id not a number`);
+                            }   
+                        }
+                    }
                 }
             }
             catch(e)
@@ -52,7 +59,7 @@ function register_client_route(app, url, sql_builder){
                 var request = new sql.Request();
                 
                 // query to the database and get the records
-                request.query(sql_builder(client_id), 
+                request.query(sql_builder(req.params), 
                     function (err, results) {
                     
                         if (err) {
@@ -68,21 +75,23 @@ function register_client_route(app, url, sql_builder){
     );
 }
 
-register_client_route(app,
-                '/addresses/:client_id',
-                client_id => 
+register_route(app,
+                "addresses",
+                {"client_id":"Int"},
+                params => 
                     `select address_id, company_id, company_type, company_name, branch_type, Branch,
                         Member, Attention, address_1, address_2, address_3, 
                         City, State, Country, Zip, cast(case when notes is not null then notes else '' end as varchar(MAX)) as Notes, 
                         case when ol.seq_num is not null then ol.seq_num else 4 end as sort_key  
                         from rpm_client_address ca 
                         left outer join rpm_option_list ol on ol.application = 'AMECLIENTMGT' and ol.option_type = 'BRANCH_TYPE' and ol.option_value = ca.branch_type 
-                        WHERE company_type = 'C' AND company_id = ${client_id} 
+                        WHERE company_type = 'C' AND company_id = ${params.client_id} 
                         order by sort_key, address_1`);
 
-register_client_route(app,
-                '/locations/:client_id',
-                client_id => 
+register_route(app,
+                "locations",
+                {"client_id":"Int"},
+                params => 
                     `select l.location_id, l.address_id, l.bill_to_address_id, l.ship_to_address_id, l.bill_to_policy, 
                     l.ship_to_policy, l.monthly_service_fee, l.months_per_bill_period, l.bill_ahead_days,
                     l.ascap_rate_plan, l.service_start_date, l.qc_call_date, l.season_start, l.season_end,
@@ -92,7 +101,7 @@ register_client_route(app,
                     l.location_status, b.billed_from_date, b.billed_through_date, b.paid_through_date,
                     l.charge_template_id 
                     FROM rpm_client_location l 
-                    INNER JOIN qry_location_billing_dates b ON l.location_id = b.location_id WHERE l.client_id = ${client_id}`);
+                    INNER JOIN qry_location_billing_dates b ON l.location_id = b.location_id WHERE l.client_id = ${params.client_id}`);
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
